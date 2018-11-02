fcea329a62d7d46ad9584e0e883863e29e49b3ab
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-11-02 16:40:00 +0700

    Add flow-type

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
 
-class App extends Component {
+class App extends Component<{}> {
   render() {
     return (
       <div className="App">
@@ -11,6 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Home />
           <a
             className="App-link"
             href="https://reactjs.org"
