610262c0f0adbd538c58171006ba598b3193a571
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-11-02 16:44:31 +0700

    Remove @flow from App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// @flow
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home";
 
-class App extends Component<{}> {
+class App extends Component {
   render() {
     return (
       <div className="App">
